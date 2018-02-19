484ecae13fbe89ab9d722bf63a03fa4fa6041336
mrbernnz <noel.sagaille@gmail.com>
2018-02-19 16:14:49 -0500

    * Add postcss packages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
+const postCssImport = require('postcss-import')();
+const cssNext = require('postcss-cssnext')();
+const cssNano = require('cssnano')();
+const reporter = require('postcss-reporter')();
+
 module.exports = {
   siteMetadata: {
     title: 'Noel and Tricia Website'
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-postcss-sass']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-postcss-sass',
+      options: {
+        postCssPlugins: [postCssImport, cssNext, cssNano, reporter],
+        precision: 8
+      }
+    }
+  ]
 };
