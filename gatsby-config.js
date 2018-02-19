2b15d6bdc4e30006e25564f098df64e45dc16e1a
mrbernnz <noel.sagaille@gmail.com>
2018-02-19 16:15:18 -0500

    * Add 📦:  gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
       resolve: 'gatsby-plugin-postcss-sass',
       options: {
         postCssPlugins: [postCssImport, cssNext, cssNano, reporter],
