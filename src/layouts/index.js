4d02f90474530ba5af63faa5d8393465c842e31f
mrbernnz <noel.sagaille@gmail.com>
2018-06-17 15:58:29 -0400

    * Wrap layout in material-ui

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import styled from 'styled-components';
+import { withStyles } from '@material-ui/core/styles';
+import withRoot from '../withRoot';
 
 import '../font.css';
 
-const Container = styled.div`
-  box-sizing: border-box;
-  font-family: 'Brandon Grotesque Regular';
-  line-height: 1.6;
-  text-transform: none;
-  letter-spacing: normal;
-  font-size: 16.8px;
-  color: #929492;
-  font-variant-ligatures: none;
-`;
+function styles(theme) {
+  return {
+    container: {
+      'box-sizing': 'border-box',
+      'font-family': 'Brandon Grotesque Regular',
+      'line-height': 1.6,
+      'text-transform': 'none',
+      'letter-spacing': 'normal',
+      'font-size': '16.8px',
+      color: '#929492',
+      'font-variant-ligatures': 'none'
+    }
+  };
+}
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({ classes, children }) => (
   <div>
     <Helmet>
       <title>Noel &#38; Tricia Wedding Website</title>
       <meta name="description" content="Our wedding website" />
       <meta name="keywords" content="wedding, noel sagaille, tricia quartey" />
     </Helmet>
-    <Container>{children()}</Container>
+    <div className={classes.container}>{children()}</div>
   </div>
 );
 
@@ -31,4 +36,4 @@ TemplateWrapper.propTypes = {
   children: PropTypes.func
 };
 
-export default TemplateWrapper;
+export default withRoot(withStyles(styles)(TemplateWrapper));
